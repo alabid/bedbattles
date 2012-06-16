ba7146b8de08ed445fdf801646ff4d7f1821d16b
jpwright <jpwright0@gmail.com>
2012-06-16 10:24:35 -0500

    register with post data

--- app.py
+++ app.py
@@ -12,12 +12,12 @@ def hello():
 @app.route('/register/', methods=['GET', 'POST'])
 def register():
 	try:
-	       	# username = request.form['username']
-       		# uid = request.form['uid']
-		# email = request.form['email']
-		username = "test"
-		uid = 123456789
-		email = "testemail"
+	       	username = request.form['username']
+       		uid = request.form['uid']
+		email = request.form['email']
+		#username = "test"
+		#uid = 123456789
+		#email = "testemail"
 		post = {"name": username, "uid": uid, "email": email}
 		connection = Connection("mongodb://heroku:54cce0fe06c2ec87c6c0ede29923b6e0@flame.mongohq.com:27028/app5293195")
 		db = connection.app5293195
