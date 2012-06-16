0947c4f35a0ee52045e005782a9a388eaab21e7b
Daniel Alabi <alabid@carleton.edu>
2012-06-16 10:01:22 -0400

    pushing new changes made by phil

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+import flask
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,4 +10,4 @@ def hello():
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000))
-	app.run(host='0.0.0.0', port=port)
+	app.run(debug=True)
