df718f78009b943b8d772ac12909b739bfca7d1f
jpwright <jpwright0@gmail.com>
2012-06-16 10:00:33 -0500

    fixing mongo integration

--- app.py
+++ app.py
@@ -12,15 +12,14 @@ def hello():
 @app.route('/register/', methods=['GET', 'POST'])
 def register():
 	try:
-		if request.method == 'GET':
-			# username = request.form['username']
-			# uid = request.form['uid']
-			# email = request.form['email']
-			post = {"name": "Python", "uid": 987654321, "email": "py@py.org"}
-			connection = Connection('localhost', 27028)
-			db = connection.app5293195
-			collection = db.users
-			users.insert(post)
+	       	# username = request.form['username']
+       		# uid = request.form['uid']
+		# email = request.form['email']
+		post = {"name": "Python", "uid": 987654321, "email": "py@py.org"}
+		connection = Connection('localhost', 27028)
+		db = connection.app5293195
+		collection = db.users
+		users.insert(post)
 	except:
 		return "failure"
 	finally:
