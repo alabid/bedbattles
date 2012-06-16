12834d87df825d4a4ce58de642e8e6c95ebc7213
hackathon <hackathon@alabidan.(none)>
2012-06-16 09:16:41 -0500

    jason: updating mongo test

--- app.py
+++ app.py
@@ -1,26 +1,28 @@
 import os
 
 from flask import Flask
+from flask.ext.pymongo import PyMongo
 from pymongo import Connection
 app = Flask(__name__)
+app.config['MONGO_DBNAME'] = 'app5293195'
+mongo = PyMongo(app, config_prefix='MONGO')
 
 @app.route('/')
 def hello():
-	return 'Hello World!'
+	return flask.render_template("index.html")
 
 @app.route('/register/', methods=['POST'])
-def register(request):
-	if request.method == 'POST':
-		username = request.form['username']
-		uid = request.form['uid']
-		email = request.form['email']
-
-		connection = Connection('localhost', 27028)
-		db = connection.app5293195
-		users = db.users
-		post = {"name": "Python", "uid": 987654321, "email": "py@py.org"}
-		users.insert(post)
-		return flask.render_template("index.html")
+def register():
+	try:
+		if request.method == 'POST':
+			username = request.form['username']
+			uid = request.form['uid']
+			email = request.form['email']
+			post = {"name": "Python", "uid": 987654321, "email": "py@py.org"}
+			mongo.db.users.insert(post)
+	finally:
+		return "success"
+		
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000))
