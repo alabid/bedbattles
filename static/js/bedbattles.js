ea126b4aa8c881af02478f8887d50421d662781f
Daniel Alabi <alabid@carleton.edu>
2012-06-16 12:25:28 -0400

    bla

--- static/js/bedbattles.js
+++ static/js/bedbattles.js
@@ -186,4 +186,4 @@ var _run = function() {
 
 window["bed_battles"] = new bedbattles();
 
-window.bed_battles._run(); // main method entry to application
+bed_battles._run(); // main method entry to application
