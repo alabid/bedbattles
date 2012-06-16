344e74963448153981cdb4979f6ed2b21ec952c1
jpwright <jpwright0@gmail.com>
2012-06-16 15:07:32 -0500

    stuff

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def battle(battleid, uid):
 			time = datetime.now()
 			month = time.month
 			day = time.day
-			wakeups.update({"battleid": battleid, "uid": uid, "month": month, "day": day}, {"win": "true"}, False, False)
+			wakeups.update({"battleid": battleid, "uid": uid, "month": month, "day": day}, {"$set": {"win": "true"}}, False, False)
 	else:
 	#try:
 		connection = Connection("mongodb://heroku:54cce0fe06c2ec87c6c0ede29923b6e0@flame.mongohq.com:27028/app5293195")
