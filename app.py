0491566852f03c7dd1802a0c3dfa88626c55e2dd
jpwright <jpwright0@gmail.com>
2012-06-16 10:49:36 -0500

    twilio sms

--- app.py
+++ app.py
@@ -34,15 +34,15 @@ def register():
 		
 @app.route('/sms/<uid>', methods=['GET', 'POST'])
 def sms(uid):
-	try:
+	#try:
 		account = "AC720f0500e36bbb4afa8e2d52e360e3ba"
 		token = "bb5de69f671b98f875277f69c0d0b497"
 		client = TwilioRestClient(account, token)
 
 		message = client.sms.messages.create(to="+19145884793", from_="+14155992671", body="Good morning!")
 		return "success"
-	except:
-		return "failure"
+	#except:
+		#return "failure"
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 55641))
