74acfc7d3d39782334f6458496debb90e1c66c36
jpwright <jpwright0@gmail.com>
2012-06-16 14:29:55 -0500

    add phone

--- app.py
+++ app.py
@@ -118,7 +118,7 @@ def visualize(battleid):
 	docs2 = wakeups.find({"uid": user2})
 	output += "<p>"+username2+"'s wakeups:<br />"
 	for wakeup in docs2:
-		output += str(wakeup["month"])+"/"+str(wakeup["day"])+" at "+str(wakeup["hour"])+":"+str(wakeup["minute"])
+		output += str(wakeup["month"])+"/"+str(wakeup["day"])+" at "+str(wakeup["hour"])+":"+str(wakeup["minute"]+"<br />")
 	output += "</p>"
 	return output
 
@@ -140,11 +140,13 @@ def createbattle():
 		return "success"
 	except:
 		return "failure"
-@app.route('/addphone/<uid>/<phone>')
+@app.route('/addphone/<uid>/<phone>', methods=['POST'])
 def addphone():
 	connection = Connection("mongodb://heroku:54cce0fe06c2ec87c6c0ede29923b6e0@flame.mongohq.com:27028/app5293195")
 	db = connection.app5293195
 	users = db.users
+	uid = request.form.get('currentUser')
+	phone = request.form.get('phoneNumber')
 	users.update({"uid": uid}, {"phone": phone}, False, False)
 
 if __name__ == '__main__':
